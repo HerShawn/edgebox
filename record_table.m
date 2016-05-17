45321c10b2ea32f69a4a61d26e670fc312cd6394
her shawn <hexiangman@qq.com>
2016-05-17 16:07:41 +0800

    2016.5.17
    1.种子点（即聚类中心点）做好了；
    但是有两个问题：
    1.max重复的问题；
    2.turn太小的问题；

--- record_table.m
+++ record_table.m
@@ -4,14 +4,13 @@ function record_table(island_tab)
 %在island_table计算得出聚类中心，然后在一行结束后返给col_bt_se_t_test
 global record_tab;
 global cluster_tab;
-record_tab=zeros(2,10);
-cluster_tab=zeros(10,10);
+
 
 tab_idx=size(island_tab,2);
-record_tab(2,tab_idx)=record_tab(2,tab_idx)+1;
+record_tab(1,tab_idx)=record_tab(1,tab_idx)+1;
 
 for i=1:tab_idx
-    cluster_tab(tab_idx,i)=(island_tab(1,i)+island_tab(1,i))/2;
+    cluster_tab(tab_idx,i)=round((island_tab(1,i)+island_tab(2,i))/2);
 end
 
 end
